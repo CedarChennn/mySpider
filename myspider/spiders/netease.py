e0ab0dccedfd43fbdd83636cba3cc627eeb1cec1
cedarchennn <a1249812431@gmail.com>
2019-09-17 09:09:43 +0800

    genspider netease

---
+++ myspider/spiders/netease.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class NeteaseSpider(scrapy.Spider):
+    name = 'netease'
+    allowed_domains = ['163.com']
+    start_urls = ['http://163.com/']
+
+    def parse(self, response):
+        pass
